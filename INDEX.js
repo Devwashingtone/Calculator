fdfe1f89e63637ea13cc1fb28dfb6b8d5c4dd9f1
Devwashingtone <152479481+Devwashingtone@users.noreply.github.com>
2024-03-24 22:45:19 +0300

    Add files via upload

---
+++ INDEX.js
@@ -0,0 +1,26 @@
+/*const display= document.getElementById("display").onclick; 
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value= eval(display.value)
+    }
+    catch(error){
+        display.value="Error";
+    }
+
+}
+
+element.addEventListener("onClick", appendToDisplay)
+
+function appendToDisplay(input) {
+    
+    appendToDisplay.value+= input;
+}
+*/
